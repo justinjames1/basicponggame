7830f656e7b54721610c683332f2388c84878431
Justin <justindude057@gmail.com>
2026-09-14 22:08:06 -0400

    Correct ball movement logic in ball.cpp
    Fix direction handling and add ball position updates.

--- src/ball.cpp
+++ src/ball.cpp
@@ -1,6 +1,9 @@
 int main(){
+int BX=0;
+int BY=0;
 int direction;
 int ball_angle;
-direction == 1; //move right
-  
+direction == TRUE; //move right
+if (direction==TRUE){BX++;}
+  if(direction==-1;){BX--;}
 } //initalize the pixels on the screen later.
