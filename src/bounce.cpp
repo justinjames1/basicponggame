93648398e4a01b5ef0a1716ed67d70d3d78d7a9b
Justin <justindude057@gmail.com>
2026-09-14 22:15:00 -0400

    Fix ball direction toggle logic in bounce.cpp

--- src/bounce.cpp
+++ src/bounce.cpp
@@ -1,6 +1,7 @@
 int main(){
 if (){ball_angle=-ball_angle;
-     direction=-direction;} //ball hits the slider
+if(direction==TRUE){direction==FALSE}
+if(direction==FALSE){direction==TRUE}} //ball hits the slider
 
 
 }
