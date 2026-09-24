71eea76c17aa5959150ef7d31b717bb8c4a16e91
Justin <justindude057@gmail.com>
2026-09-24 16:27:57 -0400

    Adjust ball angle and implement velocity
    Modified ball movement logic and added velocity control.

--- src/game.cpp
+++ src/game.cpp
@@ -1,18 +1,18 @@
 #include "config.h"
 int main() 
 {
-int ball_angle=2;
+int ball_angle=1;
 int x=200;
 int y=200;
 int i = 255;
 int radius = 20;
-
+int vel = 1;
 int left;
 int right;
-
+int bounce;
 Color myCustomColor = {i,i,i,255};
 Vector2 startPos = {200,0}; Vector2 endPos = {200,400};
-x+=ball_angle;y+=ball_angle; //calculate angle
+
 
  InitWindow(400, 400, "pong");
 
@@ -25,14 +25,14 @@ x+=ball_angle;y+=ball_angle; //calculate angle
 DrawLineEx(startPos,endPos, 15, myCustomColor);
 DrawCircle(x,y,radius, GRAY);
 
+left = x-radius;
+right = x+radius;
 
-
- x++;
-if (ball_angle > 1){x=+ball_angle;y=+ball_angle;}
-if (ball_angle < 1){x=-ball_angle;y=-ball_angle;}
-left = -radius;
-right = radius;
-if (left = 0){}
+ x+=vel;
+ if (right == 400){bounce = 1; vel= +vel;}
+ if(bounce == 1){ball_angle=-ball_angle;}
+if (ball_angle > 0){x+=ball_angle;y+=ball_angle;}
+if (ball_angle < 0){x-=ball_angle;y-=ball_angle;}
 void rgb_mode(){
 i-=1; 
 float frequency = (float)i / 100 * 2.0f * 3.14159265f;
